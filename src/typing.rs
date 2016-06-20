4f4f426cb1a56e7aa590be6f9d1df502795f6708
koba-e964 <koba_max@kobas-Air.pohe>
2016-06-20 14:55:10 +0900

    Add tests for typing.rs

--- src/typing.rs
+++ src/typing.rs
@@ -24,3 +24,27 @@ fn f_sub(ast: &AST, env: &mut HashMap<String, Type>) -> TypedAST {
 pub fn f(ast: &AST) -> TypedAST {
     f_sub(ast, &mut HashMap::new())
 }
+
+#[cfg(test)]
+mod tests {
+    use parse;
+    use typing;
+    use ast;
+    use ast::{AST, Op, Type};
+    #[test]
+    fn operations_test() {
+        let ast1 = AST::OpNode(Op::Sub, Box::new(AST::Num(7)), Box::new(AST::Num(4)));
+        assert_eq!(ast::ty_of_ast(&typing::f(&ast1)), Type::Int);
+        let ast2 = AST::OpNode(Op::Div, Box::new(AST::Num(20)), Box::new(AST::Num(4)));
+        assert_eq!(ast::ty_of_ast(&typing::f(&ast2)), Type::Int);
+    }
+    #[test]
+    fn letex_test() {
+        let ast1 = parse::parse("let x = 4 in x + x");
+        assert_eq!(ast::ty_of_ast(&typing::f(&ast1)), Type::Int);
+        let ast2 = parse::parse("let x = 4 in let x = 3 in x + x");
+        assert_eq!(ast::ty_of_ast(&typing::f(&ast2)), Type::Int);
+        let ast3 = parse::parse("let x = 4 in (let x = 3 in x) + x");
+        assert_eq!(ast::ty_of_ast(&typing::f(&ast3)), Type::Int);
+    }
+}
