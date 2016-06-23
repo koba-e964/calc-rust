92da7c4a780b5d066753070b271fd1bfcbb33392
koba-e964 <koba_max@kobas-Air.pohe>
2016-06-24 02:00:51 +0900

    Add docopt to dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
+#![feature(plugin)]
+#![plugin(docopt_macros)]
+
 extern crate calc;
+extern crate rustc_serialize;
+extern crate docopt;
+
 use std::io;
 use std::io::Write;
 use calc::parse;
 use calc::interpret;
 use calc::typing;
 
+docopt!(Args, "
+Usage: calc-rust [options] [INPUT]
+
+Options:
+    -v, --verbose  Verbose mode
+");
+
 fn main() {
+    let args: Args = Args::docopt()
+        .decode()
+        .unwrap_or_else(|e| e.exit());
+    if args.flag_verbose {
+        println!("verbose mode");
+    }
+    println!("input file: {}", args.arg_INPUT);
     print!("> ");
     io::stdout().flush().ok().unwrap();
     let mut s: String = "".to_string();
